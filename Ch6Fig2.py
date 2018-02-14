073f25aee4b14d71f000eb78642be6b2f757656b
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-02-13 17:41:16 -0800

    Update Ch6Fig2.py

--- Ch6Fig2.py
+++ Ch6Fig2.py
@@ -1,4 +1,3 @@
-import numpy
 import frmbook_funcs
 
 #Get Fama-French market data. Convert to log-returns
@@ -6,18 +5,9 @@ Date,market_minus_rf,SMB,HML,RF=frmbook_funcs.getFamaFrench3()
 ActualReality=frmbook_funcs.LogReturnConvert(market_minus_rf,RF)
 
 #Generate sample standard deviations
-SampleSd=[]
-Sqrt12=12.0**0.5
-for lookback in [12,36,60]:
-    Sds=[]  #Save lookback-length SD's in SdS
-    for x in range(0,len(ActualReality)-lookback):
-        StdDev=numpy.std(\
-        ActualReality[x:x+lookback])
-        Sds.append(StdDev*Sqrt12)
-    SampleSd.append(Sds)   #Add a row to SampleSd
+lookbacks=[12,36,30]
+SampleSd=GenSampleSd(ActualReality,lookbacks)
 
 #Graph
-StubOffset=6  #First six months are stub dates
-lookbacks=[12,36,60]
 colors=['y-','b-','r-']
-frmbook_funcs.PlotSampleSd('Figure 2',Date,SampleSd,StubOffset,lookbacks,colors)
+frmbook_funcs.PlotSampleSd('Figure 2',Date,SampleSd,lookbacks,colors)
