4bc41bc05c9e32222bf96d4d06088f4073d547f2
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-02-13 17:54:27 -0800

    Update Ch6Fig3.py

--- Ch6Fig3.py
+++ Ch6Fig3.py
@@ -6,12 +6,10 @@ import frmbook_funcs
 Date,market_minus_rf,SMB,HML,RF=frmbook_funcs.getFamaFrench3()
 ActualReality=frmbook_funcs.LogReturnConvert(market_minus_rf,RF)
 
-StubOffset=6
-mu=numpy.average(ActualReality[StubOffset:])
-sigma=numpy.std(ActualReality[StubOffset:])
+mu=numpy.average(ActualReality)
+sigma=numpy.std(ActualReality)
         
 #Generate histogram
-#put infinite bins at the ends
 minhist=-15
 maxhist=15
 stepsize=1.5
@@ -19,10 +17,10 @@ bins=[]
 bins.append(minhist)
 for x in range(int((maxhist-minhist)/stepsize)):
     bins.append(bins[x]+stepsize)
+            
+n, bins, patches = plt.hist(ActualReality, bins, \
+                            normed=1, facecolor='blue', alpha=0.5)
 
-n, bins, patches = plt.hist(\
-ActualReality[StubOffset:], bins, normed=1, facecolor='blue', alpha=0.5)
- 
 # add a 'best fit' line
 y = mlab.normpdf(bins, mu, sigma)
 plt.plot(bins, y, 'r--')
