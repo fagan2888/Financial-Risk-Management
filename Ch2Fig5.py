1822e55eda6f192e5de93366abdae6ab0655be9b
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-11 13:16:47 -0700

    Update Ch2Fig5.py

--- Ch2Fig5.py
+++ Ch2Fig5.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
-tenors=[0.083333333,0.25,0.5,1,2,3,5,7,10,20,30]
-#Curve source:
-#https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield
-curve20010102=[None,5.87,5.58,5.11,4.87,4.82,
-               4.76,4.97,4.92,5.46,5.35]
+#Show the inverted Treasury curve from
+#2001-01-02
+
+targetdate='2001-01-02'
+tenorsfromtsy,seriesnames,cdates,ratematrix=GetUSCurve()
+curvetarget=ratematrix[cdates.index(targetdate)]
                                         
-plt.plot(tenors, curve20010102)
+plt.plot(tenorsfromtsy, curvetarget)
 ## Configure the graph
-plt.title('US Treasury Curve 2001-01-02')
-plt.ylim(0,7)
+plt.title('US Treasury Curve '+targetdate)
+plt.ylim(0,max([x for x in curvetarget if pd.notna(x)])+.5)
 plt.xlabel('Tenor')
 plt.ylabel('Rate')
 plt.grid(True)
