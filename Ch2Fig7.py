777ae49f7fbe974bf96c7a7b7c662ec9c76be7e5
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-11 13:01:42 -0700

    Update Ch2Fig7.py

--- Ch2Fig7.py
+++ Ch2Fig7.py
@@ -1,36 +1,29 @@
 import matplotlib.pyplot as plt
-#Plot a short rate curve
-tenornames=['1mo','3mo','6mo','1yr','2yr','3yr',
-        '5yr','7yr','10yr','20yr','30yr']
-tenornumbers=range(len(tenornames))
-tenors=[0.083333333,0.25,0.5,1,2,3,5,7,10,20,30]
-#Curve source:
-#https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield
-curve2017=[1.28,1.39,1.53,1.76,1.89,1.98,
-           2.2,2.33,2.4,2.58,2.74]
-curve=curve2017
-yeartext='2017'
-                                             
-plt.plot(tenornumbers, curve, label=yeartext)
-#Bootstrap a short rate curve
+import pandas as pd
+#Get a yearend US Treasury curve
+#Interpolate it to monthly and compute a short
+#rate curve based on that
+#Plot both
 
-shortrates=[]
-for i,rate in enumerate(curve):
-    short=curve[i]    
-    if (i!=0):
-        denom=tenors[i]-tenors[i-1]
-        numer=curve[i]-curve[i-1]
-        if (denom!=0):
-            short+=tenors[i-1]*numer/denom
-    shortrates.append(short)
+tenorsfromtsy,seriesnames,cdates,ratematrix=GetUSCurve()
+#find end of last year
+t=pd.Timestamp.now()
+for day in [31,30,29,28]:
+    lastday=str(t.year-1)+'-12-'+str(day)
+    if lastday in cdates:
+       break
 
-plt.plot(tenornumbers, shortrates, label='Short')
+#Get monhtly interpolated curve and short rate curve
+curvefromtsy=ratematrix[cdates.index(lastday)]
+tenors,curvemonthly,shortrates=InterpolateCurve(tenorsfromtsy,curvefromtsy)
+
+plt.plot(tenors, curvemonthly, label=lastday)
+plt.plot(tenors, shortrates, label='Short')
 ## Configure the graph
 plt.title('US Treasury and Short Rate Curve')
 plt.xlabel('Rate')
 plt.ylabel('Tenor')
-plt.ylim(0,3)
-plt.xticks(tenornumbers,tenornames)
+plt.ylim(0,max(curvemonthly)+.5)
 plt.legend()
 plt.grid(True)
 plt.show
