5521633b7f3be2cc376ecbc9c179157b27012666
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-11 17:35:00 -0700

    Update Ch2Fig2.py

--- Ch2Fig2.py
+++ Ch2Fig2.py
@@ -1,28 +1,8 @@
-def formula2p8(c,r,t):
-    #Formula 2.8 for Macauley duration of bond
-    #with annual coupon c, t years to
-    #maturity, discount rate r
-    if r<=-100:  #Unreasonable discount rate
-        return(0)
-    y=1/(1+r/100)
-    ytothet=y**t
-    duration=100*t*ytothet
-    if (y==1):   #no discount rate
-        multiplier=t*(t+1)/2
-    else:
-        multiplier=(1-ytothet-t*(1-y)*ytothet)/(1-y)**2
-    duration+=multiplier*c*y
-    #formula2p3 is in Ch2Fig1.py
-    price=formula2p3(c,r,t)   #Rescale by pricve
-    duration/=price
-    return(duration)
-#Done with Formula2p8
-    
 import matplotlib.pyplot as plt                                                 
 import numpy as np
-
-## Create functions and set domain length
-x = np.arange(0, 10.25, .25)
+#Plot duration of a newly issued bullet bond
+#with same coupon and discount rate
+x = np.arange(0, 14.25, .25)
 years=30
 y=[]
 for rate in (x):
@@ -34,5 +14,6 @@ plt.plot(x, y)
 plt.title('Duration - Formula 2.8')
 plt.xlabel('Discount Rate')
 plt.ylabel('Duration')
+plt.xlim(0,14)
 plt.grid(True)
 plt.show()
