653efd138fa1e2abaf4484b176019184db0467a0
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-11 17:29:14 -0700

    Update Ch2Fig1.py

--- Ch2Fig1.py
+++ Ch2Fig1.py
@@ -1,24 +1,8 @@
-def formula2p3(c,r,t):
-    #Formula 2.3 for price of bond
-    #with annual coupon c, t years to
-    #maturity, discount rate r
-    if r<=-100:  #Unreasonable discount rate
-        return(100)
-    y=1/(1+r/100)
-    price=100*(y**t)
-    if (y==1):   #no discount rate
-        geometric=t
-    else:
-        geometric=(1-y**t)/(1-y)
-    price+=geometric*c*y
-    return(price)
-#Done with Formula2p3
-
 import matplotlib.pyplot as plt                                                 
 import numpy as np
-
-## Create functions and set domain length
-x = np.arange(0, 10.25, .25)
+# Plot bond prices using Formula 2.3
+# Function formula2p3 is in frmbook_funcs
+x = np.arange(0, 14.25, .25)
 coupon=7
 years=29
 y=[]
@@ -30,5 +14,6 @@ plt.plot(x, y)
 plt.title('Bond Price - Formula 2.3')
 plt.xlabel('Discount Rate')
 plt.ylabel('Price')
+plt.xlim(0,14)
 plt.grid(True)
 plt.show()
