6c50cd2d7969d0c9dbca586b2c63b3093fe849e5
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-13 11:19:27 -0700

    Update Ch2Fig5.py

--- Ch2Fig5.py
+++ Ch2Fig5.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 #Show the inverted Treasury curve from
 #2001-01-02
 
 targetdate='2001-01-02'
-tenorsfromtsy,seriesnames,cdates,ratematrix=GetUSCurve()
-curvetarget=ratematrix[cdates.index(targetdate)]
-                                        
-plt.plot(tenorsfromtsy, curvetarget)
+#Note no one-month rate on this date
+seriesnames=['DGS3MO','DGS6MO','DGS1',
+             'DGS2','DGS3','DGS5','DGS7',
+             'DGS10','DGS20','DGS30']
+#GetFREDMatrix is in frmbook_funcs
+cdates,ratematrix=GetFREDMatrix(seriesnames,startdate=targetdate,enddate=targetdate)
+
+tenors=[0.25,0.5,1,2,3,5,7,10,20,30]
+
+plt.plot(tenors, ratematrix[0])
 ## Configure the graph
 plt.title('US Treasury Curve '+targetdate)
-plt.ylim(0,max([x for x in curvetarget if pd.notna(x)])+.5)
-plt.xlabel('Tenor')
-plt.ylabel('Rate')
+plt.ylim(0,max([x for x in ratematrix[0] if pd.notna(x)])+.5)
+plt.xlabel('Tenor (years)')
+plt.ylabel('Rate (%/year)')
 plt.grid(True)
 plt.show
