04a8b0fcabc8877214634ec83263e254c7dbb2f2
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-13 11:54:46 -0700

    Update Ch2Fig7.py

--- Ch2Fig7.py
+++ Ch2Fig7.py
@@ -5,24 +5,36 @@ import pandas as pd
 #rate curve based on that
 #Plot both
 
-tenorsfromtsy,seriesnames,cdates,ratematrix=GetUSCurve()
 #find end of last year
 t=pd.Timestamp.now()
 for day in [31,30,29,28]:
     lastday=str(t.year-1)+'-12-'+str(day)
-    if lastday in cdates:
+    if pd.Timestamp(lastday).weekday()<5:
        break
 
+seriesnames=['DGS1MO','DGS3MO','DGS6MO','DGS1',
+             'DGS2','DGS3','DGS5','DGS7',
+             'DGS10','DGS20','DGS30']
+#GetFREDMatrix is in frmbook_funcs
+cdates,ratematrix=GetFREDMatrix(seriesnames,startdate=lastday,enddate=lastday)
+
+tenorsfromtsy=[]
+for i in range(len(seriesnames)):
+    if seriesnames[i][-2:]=='MO':
+        tenorsfromtsy.append(float(seriesnames[i][3:-2])/12)
+    else:
+        tenorsfromtsy.append(float(seriesnames[i][3:]))
+
 #Get monhtly interpolated curve and short rate curve
-curvefromtsy=ratematrix[cdates.index(lastday)]
-tenors,curvemonthly,shortrates=InterpolateCurve(tenorsfromtsy,curvefromtsy)
+#InterpolateCurve is in frmbook_funcs
+tenors,curvemonthly,shortrates=InterpolateCurve(tenorsfromtsy,ratematrix[0])
 
 plt.plot(tenors, curvemonthly, label=lastday)
 plt.plot(tenors, shortrates, label='Short')
 ## Configure the graph
-plt.title('US Treasury and Short Rate Curve')
-plt.xlabel('Rate')
-plt.ylabel('Tenor')
+plt.title('US Treasury and Short Rate Curves')
+plt.xlabel('Tenor (years)')
+plt.ylabel('Rate (%/year)')
 plt.ylim(0,max(curvemonthly)+.5)
 plt.legend()
 plt.grid(True)
