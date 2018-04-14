4edca5eba97430cd4e5a9773b69b9c75b26a1d81
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-14 12:46:24 -0700

    Update Ch2Fig5.py

--- Ch2Fig5.py
+++ Ch2Fig5.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from frmbook_funcs import GetFREDMatrix
+from frmbook_funcs import TenorsFromNames
 #Show the inverted Treasury curve from 2001-01-02
 
 targetdate='2001-01-02'
@@ -7,19 +9,9 @@ targetdate='2001-01-02'
 seriesnames=['DGS3MO','DGS6MO','DGS1',
              'DGS2','DGS3','DGS5','DGS7',
              'DGS10','DGS20','DGS30']
-#GetFREDMatrix is in frmbook_funcs
 cdates,ratematrix=GetFREDMatrix(seriesnames,startdate=targetdate,enddate=targetdate)
-rtdate=lastday,enddate=lastday)
 
-#Extract numerical (yearly) tenors from series names
-tenors=[]
-for i in range(len(seriesnames)):
-    if seriesnames[i][-2:]=='MO':
-        tenors.append(float(seriesnames[i][3:-2])/12)
-    else:
-        tenors.append(float(seriesnames[i][3:]))
-
-plt.plot(tenors, ratematrix[0])
+plt.plot(TenorsFromNames(seriesnames), ratematrix[0])
 ## Configure the graph
 plt.title('US Treasury Curve '+targetdate)
 plt.ylim(0,max([x for x in ratematrix[0] if pd.notna(x)])+.5)
