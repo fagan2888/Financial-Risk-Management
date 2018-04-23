193466fccef8b61317d53f4bee7dc89fbaecc440
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-04-23 11:46:28 -0700

    Update Ch4Fig2.py

--- Ch4Fig2.py
+++ Ch4Fig2.py
@@ -1,4 +1,54 @@
-#To be run after Ch4Fig1.py has pulled in data
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+from frmbook_funcs import LastYearEnd
+from frmbook_funcs import GetFREDMatrix
+#Get 3 currencies until the end of
+#previous year. Form 3x3 covariance matrix.
+#Show Q-Q plot of CHF
+#Print Jarque-Bera Statistics
+#Print table of normal probabilities
+
+lastday=LastYearEnd()
+seriesnames=['DEXSZUS','DEXUSUK','DEXJPUS']
+cdates,ratematrix=GetFREDMatrix(seriesnames,
+            enddate=lastday)
+
+oldstate=np.seterr(all='ignore')
+#Convert levels to log-returns
+#First take logs of the currency levels
+#Swissie and yen are in currency/dollar, but
+#the Pound is dollar/currency. Reverse sign
+#of the Pound.
+multipliers=[-1,1,-1]
+lgrates=[]
+for i in range(len(ratematrix)):
+    lgrates.append(list(np.log(ratematrix[i])))
+    lgrates[i]=list(np.multiply(lgrates[i],multipliers))
+#take differences of the logs.
+#get rid of any where Yen doesn't have data
+difflgs=[]
+lgdates=[]
+idxold=0   #Keeps track of the last good time period
+for i in range(1,len(ratematrix)):    
+    x=list(np.subtract(lgrates[i],lgrates[idxold]))
+    if pd.notna(x[2]):
+        difflgs.append(x)
+        lgdates.append(cdates[i])
+        idxold=i
+np.seterr(**oldstate)
+
+#compute covariance matrix
+c=np.cov(np.array(difflgs).T)
+
+#get eigenvalues and eigenvectors
+evals,evecs=np.linalg.eig(c)
+#Put the eigensystem in decreasing
+#order of eigenvalues
+sortorder=evals.argsort()[::-1]
+evals=evals[sortorder]
+evecs=evecs[:,sortorder]
 
 #Make the Q-Q plot of Swiss francs
 chf=[row[0] for row in difflgs]
@@ -22,7 +72,22 @@ plt.annotate('September 6, 2011', xy=(min(x), bigminus), xytext=(.5*min(x), bigm
             arrowprops=dict(facecolor='black', shrink=0.02),
             )
 ## Configure the graph
-plt.title('Q-Q plot, CHF 1999-'+lastday[:4])
+lgdates
+plt.title('Q-Q plot, CHF '+lgdates[0][:4]+'-'+lastday[:4])
 plt.ylabel('Standardized Log-return')
 plt.grid(True)
 plt.show
+
+#Jarque-Bera
+from scipy.stats import skew, kurtosis
+sk=skew(chf)
+ku=kurtosis(chf)
+jb=(nobs/6)*(sk**2+(ku**2)/4)
+print('Skewness %f' % sk)
+print('Excess Kurtosis %f' % ku)
+print('Jarque-Bera Statistic %f' % jb)
+
+#Normal distribution probabilities
+print('Normal distribution probabilities (log10)')
+for i in range(21):
+    print(i,np.log(norm.cdf(-i))/np.log(10))
