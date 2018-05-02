600e4066c90bcaa610157e72b7194168bf6d92c7
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-05-01 19:35:49 -0700

    Update Ch6Fig2.py

--- Ch6Fig2.py
+++ Ch6Fig2.py
@@ -5,9 +5,12 @@ Date,market_minus_rf,SMB,HML,RF=frmbook_funcs.getFamaFrench3()
 ActualReality=frmbook_funcs.LogReturnConvert(market_minus_rf,RF)
 
 #Generate sample standard deviations
-lookbacks=[12,36,30]
-SampleSd=GenSampleSd(ActualReality,lookbacks)
+lookbacks=[12,36,60]
+SampleSd=frmbook_funcs.GenSampleSd(ActualReality,lookbacks)
 
 #Graph
 colors=['y-','b-','r-']
-frmbook_funcs.PlotSampleSd('Figure 2',Date,SampleSd,lookbacks,colors)
+tstr='Figure 2, Sample US Stock Mkt σ, '
+tstr+=str(Date[0])[:4]
+tstr+='-'+str(Date[len(Date)-1])[:4]
+frmbook_funcs.PlotSampleSd(tstr,Date,SampleSd,lookbacks,colors)
