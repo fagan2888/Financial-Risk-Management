3d084203b8079446986b5f8979a358cc7b20224c
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-05-01 19:59:58 -0700

    Update and rename Ch6Fig3.py to Ch6Fig2a.py

--- Ch6Fig2a.py
+++ Ch6Fig2a.py
@@ -1,31 +1,30 @@
-import numpy
-import matplotlib.mlab as mlab
+import numpy as np
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import frmbook_funcs
 
 Date,market_minus_rf,SMB,HML,RF=frmbook_funcs.getFamaFrench3()
 ActualReality=frmbook_funcs.LogReturnConvert(market_minus_rf,RF)
 
-mu=numpy.average(ActualReality)
-sigma=numpy.std(ActualReality)
+mu=np.average(ActualReality)
+sigma=np.std(ActualReality)
         
 #Generate histogram
 minhist=-15
 maxhist=15
 stepsize=1.5
-bins=[]
-bins.append(minhist)
-for x in range(int((maxhist-minhist)/stepsize)):
-    bins.append(bins[x]+stepsize)
+bins=np.arange(minhist,maxhist+stepsize,stepsize)
             
 n, bins, patches = plt.hist(ActualReality, bins, \
-                            normed=1, facecolor='blue', alpha=0.5)
+                            facecolor='blue', alpha=0.5)
 
 # add a 'best fit' line
-y = mlab.normpdf(bins, mu, sigma)
+y = norm.pdf(bins,loc=mu,scale=sigma)
+y*=np.sum(n)/np.sum(y)
 plt.plot(bins, y, 'r--')
 plt.title(r'Figure 3')
  
 # Tweak spacing to prevent clipping of ylabel
 plt.subplots_adjust(left=0.15)
+plt.grid()
 plt.show()
