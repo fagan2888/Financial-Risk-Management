d474f1f2bbeeb37e077e64d9fd5456e373ba93b2
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-05-30 17:00:38 -0700

    Update Ch8Fig2.py

--- Ch8Fig2.py
+++ Ch8Fig2.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#The data
+#Data from Moody's Corporate Default and Recovery Rates,
+#1920-2017 Exhibit 32 (1-year) and Barclays Capital,
+#“The Corporate Default Probability Model,” April 2009
 ratings=['AAA','AA','A','BBB','BB','B','≤CCC']
-moodys=[0,.058,.092,.269,1.029,3.191,10.541]
+moodys=[0,.06,.08,.26,1.21,3.42,10.11]
 edf2009=[.0093,.0264,.1198,.4393,1.852,10.22,33.904]
 indices = [1,2,3,4,5,6,7]
 #Calculate optimal width
@@ -16,5 +18,5 @@ ax.bar(indices,edf2009,width,color='r',label='Ymax')
 plt.grid()
 plt.xticks(indices,ratings)
 plt.yscale('log')
-plt.title("Default rates, Moody's historical 1920-2016 and Barclays model 2009")
+plt.title("Default rates, Moody's historical 1920-2017 and Barclays model 2009")
 plt.show()
