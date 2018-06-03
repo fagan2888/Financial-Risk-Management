8dbb25b38883e85bfc4df70a156a8eaac86ec010
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-06-02 19:52:22 -0700

    Create Ch9Fig1.py

---
+++ Ch9Fig1.py
@@ -0,0 +1,15 @@
+#Show numbr-of-bottles curve for Siegel's paradox
+
+dollars_per_euro=1.17
+x=np.arange(dollars_per_euro-.3,dollars_per_euro+.3+.01,.01)
+z=x/1.17
+y=z+1/z
+
+i=np.arange(len(x))
+plt.plot(i,y,color='red')
+plt.xticks(i[::10],x[::10])
+plt.xlabel("Dollars per Euro")
+plt.ylabel("Millions of bottles")
+plt.title("Total bottle volume of Philippe-Ken Wine Company")
+plt.grid()
+plt.show()
