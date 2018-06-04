781320360f5f50ca877b0ab628947b8af019a969
kjwinston52 <35860357+kjwinston52@users.noreply.github.com>
2018-06-04 09:42:51 -0700

    Create Ch9Fig6.py

---
+++ Ch9Fig6.py
@@ -0,0 +1,60 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import FormatStrFormatter
+from scipy.stats import norm
+import numpy as np
+
+#Assumptions for this graph
+ratio=1
+start_l=2
+start_s=1
+logterm=np.log(ratio*start_s/start_l)
+time=1
+alpha_l=300
+alpha_s=-200
+rho=.5
+
+def Form9p41(lt,t,alphl,alphs,sl,ss,rh):
+    #Compute formula 9.41
+
+    #First compute sigma from 9.37
+    bigsigma_sq=sl**2-2*rh*sl*ss+ss**2
+    bigsigma_sq/=10000    #convert to fractions from bps
+    #Form numerator of D1
+    gamma_l=(alphl-sl**2/2)/10000
+    gamma_s=(alphs-ss**2/2)/10000
+    d1=lt-(gamma_l-gamma_s)*t
+    #Divide by denominator
+    d1/=np.sqrt(bigsigma_sq*t)
+    #d1 is a number of standard deviations.
+    biga=(alphl-alphs+(ss**2)/2-rh*sl*ss)/10000
+    d2=d1+(biga-bigsigma_sq/2)*np.sqrt(t)/np.sqrt(bigsigma_sq)
+    more_pain=np.exp(-lt*(1-2*biga/bigsigma_sq))*norm.cdf(d2)
+    #Apply cumulative normal
+    return(norm.cdf(d1)+more_pain)
+
+# Make data.
+X = np.arange(1,30.25,.25)  #Short sigma
+Y = np.arange(1,30.25,.25)  #Long sigma
+X, Y = np.meshgrid(X, Y)
+
+#Do Z axis in percent
+Z=100.0*Form9p41(logterm,time,alpha_l,alpha_s,Y,X,rho)
+
+# Plot the surface.
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+fig.gca().invert_xaxis()
+
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+#ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.01f'))
+
+ax.set_xlabel(r'$\sigma_S$')
+ax.set_ylabel(r'$\sigma_L$')
+ax.set_zlabel('Failure Prob (%)')
+plt.show()
